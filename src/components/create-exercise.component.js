833dd8a7be4a83876a8e4d83531d695313d4d49f
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-06-09 21:25:13 -0700

    backend set up , working on front-end

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ExercisesList(){
+    return(
+        <div>
+            <p>You are on the Create Exercise component!</p>
+        </div>
+    )
+}
